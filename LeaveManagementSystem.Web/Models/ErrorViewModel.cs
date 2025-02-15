f2e116c80ba2aafab458fdaa9d6fbb92829b5100
Your Name <your-email@example.com>
2025-02-15 12:52:33 +0300

    Added dark navbar to layot

--- LeaveManagementSystem.Web/Models/ErrorViewModel.cs
+++ LeaveManagementSystem.Web/Models/ErrorViewModel.cs
@@ -3,7 +3,7 @@ namespace LeaveManagementSystem.Web.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
+
 }
